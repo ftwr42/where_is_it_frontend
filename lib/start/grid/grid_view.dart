207760ca1c2880e76d8f3140bc65c29a24a0a572
ftwr42 <64641711+ftwr42@users.noreply.github.com>
2024-01-01 15:04:35 +0100

    add grid widget for more modular coding

---
+++ lib/start/grid/grid_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WiiGrid extends StatelessWidget {
+  const WiiGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+    // return SliverGrid(delegate: delegate, gridDelegate: gridDelegate);
+  }
+}
