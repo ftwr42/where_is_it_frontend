8f357a8790f8bc7b6b689e01daef3c4be3dc56f9
ftwr42 <64641711+ftwr42@users.noreply.github.com>
2024-01-27 14:20:56 +0100

    added map to tree

---
+++ lib/drawer/drawer_state.dart
@@ -0,0 +1,3 @@
+import 'package:where_is_it/zz_stateholder/stateconfig.dart';
+
+class DrawerState extends StateConfig {}
--- lib/zz_stateholder/state_holder_tree.dart
+++ lib/zz_stateholder/state_holder_tree.dart
@@ -1,16 +1,18 @@
 import 'package:where_is_it/zz_stateholder/stateconfig.dart';
 
 class StateHolderTree<T extends StateConfig> {
-  late String nodeName;
+  final String nodeName;
   late StateHolderTree parent;
 
   final T stateConfig;
   final List<StateHolderTree> children = [];
+  final Map<String, StateHolderTree> childMap = {};
 
   StateHolderTree({required this.stateConfig, required this.nodeName});
 
   void addChild(StateHolderTree child) {
     child.parent = this;
     children.add(child);
+    childMap.update(child.nodeName, (value) => child);
   }
 }
