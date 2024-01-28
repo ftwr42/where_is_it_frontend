031a3be6518f56ae1531b2c33ec7a5648992cb53
ftwr42 <64641711+ftwr42@users.noreply.github.com>
2024-01-28 10:10:04 +0100

    new root management

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:where_is_it/routing/rot.dart';
-import 'package:where_is_it/singleton.dart';
 import 'package:where_is_it/store/explorer/explorer_view.dart';
 import 'package:where_is_it/wii_state.dart';
 
 void main() {
-  Singleton.getInstance();
   runApp(WiiApp());
 }
 
@@ -15,8 +13,6 @@ class WiiApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var root = Singleton.root?.state as WiiState;
-
     return GetMaterialApp(
       theme: root.theme,
       home: ExplorerView(),
